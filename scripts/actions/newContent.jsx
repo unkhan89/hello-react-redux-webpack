853a91738222f891f159d6c1d366c59724e1c171
Khan <umarkh@amazon.com>
2017-06-11 16:18:29 -0700

    improved comments

--- scripts/actions/newContent.jsx
+++ scripts/actions/newContent.jsx
@@ -1,6 +1,9 @@
 
 // An 'action creator' function that returns the 'action' (some new data)
 //
+// This is a function that a user action will invoke.
+// For example, an onClick on a button can bind to this function.
+//
 export const addNewContentActionCreator = (newContent) => {
 
   console.log('actions.newContent#addNewContent() newContent: '
--- scripts/reducers/new-content.jsx
+++ scripts/reducers/new-content.jsx
@@ -1,6 +1,9 @@
 
-// TODO
+// A reducer function that will be called when an action creator is executed
+// (recall an action creator is invoked by something like a button onClick in a component)
 //
+// If the action.type matches something this function can handle, it will return
+// the data that was passed along with the action.
 //
 export default function(state=null, action) {
 
